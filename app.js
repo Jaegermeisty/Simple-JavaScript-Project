2efdc82b4ff23cb973a61916170c3d530759f362
Mathias JP <175939829+Jaegermeisty@users.noreply.github.com>
2025-03-25 16:40:07 +0100

    Add falling transtition when you remove a todo

--- app.js
+++ app.js
@@ -36,7 +36,10 @@ function deleteCheck(e) {
 
   if (item.classList[0] === 'trash-btn') {
     const todo = item.parentElement;
-    todo.remove();
+    todo.classList.add('fall');
+    todo.addEventListener('transitionend', function() {
+      todo.remove();
+    });
   }
 
   if (item.classList[0] === 'complete-btn') {
